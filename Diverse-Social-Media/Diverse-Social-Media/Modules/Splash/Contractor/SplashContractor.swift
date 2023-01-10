16993fe60cf42091f60fc603b37b4a2e94e840e4
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-01-10 23:44:44 +0300

    project folder structure has changed.

--- Diverse-Social-Media/Diverse-Social-Media/Modules/Splash/Contractor/SplashContractor.swift
+++ Diverse-Social-Media/Diverse-Social-Media/Modules/Splash/Contractor/SplashContractor.swift
@@ -8,44 +8,30 @@
 import Foundation
 
 protocol PSplashPresenterToView: PresenterToView {
-    
     func setLanguage(language: String)
-    
     func setGestureRecognizerForLanguage()
-    
     func setupPicker()
-    
     func becomeFirstResponderLanguageTextField()
-    
     func resignFirstResponderLanguageTextField()
 }
 
 protocol PSplashViewToPresenter: ViewToPresenter {
-    
     func navigateToLogin()
-    
     func navigateToRegister()
-    
     func openLanguagePicker()
-    
     func selectLanguageFromPicker(index: Int)
 }
 
 protocol PSplashPresenterToInteractor {
-    
     func fetchConfigData()
 }
 
 protocol PSplashInteractorToPresenter {
-    
     func onSuccessConfig(response: Config)
-    
     func onErrorConfig(error: BaseError)
 }
 
 protocol PSplashPresenterToRouter {
-   
     func openLoginVC()
-    
     func openRegisterVC()
 }
