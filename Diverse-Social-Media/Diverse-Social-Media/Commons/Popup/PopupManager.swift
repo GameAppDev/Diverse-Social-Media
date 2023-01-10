44f49aa2a27614f9a546caf2203d6e36cec0b5c2
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-01-10 22:16:06 +0300

    project folder structure has changed.

---
+++ Diverse-Social-Media/Diverse-Social-Media/Commons/Popup/PopupManager.swift
@@ -0,0 +1,8 @@
+//
+//  PopupManager.swift
+//  Diverse-Social-Media
+//
+//  Created by Oguzhan Yalcin on 10.01.2023.
+//
+
+import Foundation
---
+++ Diverse-Social-Media/Diverse-Social-Media/Helpers/CoreData/CoreDataManager.swift
@@ -0,0 +1,8 @@
+//
+//  CoreDataManager.swift
+//  Diverse-Social-Media
+//
+//  Created by Oguzhan Yalcin on 10.01.2023.
+//
+
+import Foundation
