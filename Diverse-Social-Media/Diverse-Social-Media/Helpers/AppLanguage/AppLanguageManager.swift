10c700f5d3668cc83016c5e91ecac52d80dc0b5f
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2023-01-10 22:17:49 +0300

    AppLanguageManager is created.

---
+++ Diverse-Social-Media/Diverse-Social-Media/Helpers/AppLanguage/AppLanguageManager.swift
@@ -0,0 +1,13 @@
+//
+//  AppLanguageManager.swift
+//  Diverse-Social-Media
+//
+//  Created by Oguzhan Yalcin on 10.01.2023.
+//
+
+import Foundation
+
+final class AppLanguageManager {
+    
+    static let shared = AppLanguageManager()
+}
